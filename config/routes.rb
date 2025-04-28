274f99b1737f138e58b13e7d1e3f9ef546a23ea3
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-04-27 19:47:31 -0700

    Create Route -  make users route

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,11 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+  resources :users, only: [:new, :create]
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your  lay out with "rake ".
 
